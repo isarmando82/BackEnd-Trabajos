f3b5ea517ee39bcad4d4a51a831b4e6d533d96ba
ismael armando <isarmando82@gmail.com>
2023-07-05 07:15:24 -0300

    Entrega 2

--- DesafioEntregable/desafioEntregable2-Armando.js
+++ DesafioEntregable/desafioEntregable2-Armando.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
+
 class ProductManager {
-  constructor() {
-    this.path = '';
+  constructor(path) {
+    this.path = path;
     this.products = [];
     this.nextProductId = 1;
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf8');
+      this.products = JSON.parse(data);
+      if (this.products.length > 0) {
+        const lastProduct = this.products[this.products.length - 1];
+        this.nextProductId = lastProduct.id + 1;
+      }
+    } catch (err) {
+      console.log('Error al cargar el archivo de productos:', err.message);
+    }
+  }
+
+  saveProducts() {
+    try {
+      const data = JSON.stringify(this.products, null, 2);
+      fs.writeFileSync(this.path, data, 'utf8');
+      console.log('Productos guardados correctamente.');
+    } catch (err) {
+      console.log('Error al guardar los productos:', err.message);
+    }
   }
 
   addProduct(product) {
@@ -18,6 +44,7 @@ class ProductManager {
 
     product.id = this.nextProductId++;
     this.products.push(product);
+    this.saveProducts();
   }
 
   getProducts() {
@@ -38,6 +65,7 @@ class ProductManager {
     if (index !== -1) {
       const product = this.products[index];
       Object.assign(product, updatedFields);
+      this.saveProducts();
       console.log('Producto actualizado correctamente.');
     } else {
       console.log('Error: Producto no encontrado.');
@@ -48,6 +76,7 @@ class ProductManager {
     const index = this.products.findIndex((p) => p.id === id);
     if (index !== -1) {
       this.products.splice(index, 1);
+      this.saveProducts();
       console.log('Producto eliminado correctamente.');
     } else {
       console.log('Error: Producto no encontrado.');
@@ -70,34 +99,35 @@ class ProductManager {
   }
 }
 
-const productManager = new ProductManager();
+const productManager = new ProductManager('products.json');
 
 // Agregar un nuevo producto.
+
 productManager.addProduct({
-  title: 'Producto 1',
-  description: 'Descripción del producto 1',
-  price: 999.99,
-  thumbnail: 'ruta/imagenProducto1.jpg',
-  code: 'ABC123',
-  stock: 10
+    title: 'Producto 1',
+    description: 'Descripción del producto 1',
+    price: 999.99,
+    thumbnail: 'ruta/imagenProducto1.jpg',
+    code: 'ABC123',
+    stock: 10
 });
 
 productManager.addProduct({
-  title: 'Producto 2',
-  description: 'Descripción del producto 2',
-  price: 100.50,
-  thumbnail: 'ruta/imagenProducto2.jpg',
-  code: 'ABC124',
-  stock: 7
+    title: 'Producto 2',
+    description: 'Descripción del producto 2',
+    price: 100.50,
+    thumbnail: 'ruta/imagenProducto2.jpg',
+    code: 'ABC124',
+    stock: 7
 });
 
 productManager.addProduct({
-  title: 'Producto 3',
-  description: 'Descripción del producto 3',
-  price: 199.99,
-  thumbnail: 'ruta/imagenProducto3.jpg',
-  code: 'ABC125',
-  stock: 5
+    title: 'Producto 3',
+    description: 'Descripción del producto 3',
+    price: 199.99,
+    thumbnail: 'ruta/imagenProducto3.jpg',
+    code: 'ABC125',
+    stock: 5
 });
 
 // Listado de todos los productos
@@ -111,4 +141,4 @@ console.log(productManager.getProductById(7));
 productManager.updateProduct(1, { price: 1099.99, stock: 15 });
 
 // Eliminar un producto
-productManager.deleteProduct(2);
+productManager.deleteProduct(3);
