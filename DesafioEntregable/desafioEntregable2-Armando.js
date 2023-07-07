36b4319990752cdfe3fc51bd1e8b4d4a8036980b
ismael armando <isarmando82@gmail.com>
2023-07-07 06:51:55 -0300

    Entregado

--- DesafioEntregable/desafioEntregable2-Armando.js
+++ DesafioEntregable/desafioEntregable2-Armando.js
@@ -1,4 +1,18 @@
-const fs = require('fs');
+// Me enconte con varias maneras de encarar y resolver este desafio, dado el
+// material suministrado y lo visto en internet. 
+// Siendo fiel al codigo suministrado en el desafio uno esta es la variante
+// que hallo correcta.
+// Luego, se podria explorar hacerlo como el ultimo HandsOnLab suministrado por
+// el profesor dado que no llegamos a desarrollarlo.
+// Entrego este que es una variante correcta y en caso que me lo indiques reentrego
+// una variante parecida al ejercicio que seguramente desarrollaremos en el after,
+// que hallo diferente pero con un resultado similar.
+// En este caso incluyo el archivo .json pero sino se crearía donde este el archivo a darle 
+// node.
+// Prefiero entregar lo más pronto posible para tener un feedback que me de tiempo a una posible 
+// reentrega, con una sintaxis parecida al ejercicio de clase que ya comienzo a hacer.
+
+const fs = require('fs').promises;
 
 class ProductManager {
   constructor(path) {
@@ -8,9 +22,9 @@ class ProductManager {
     this.loadProducts();
   }
 
-  loadProducts() {
+  async loadProducts() {
     try {
-      const data = fs.readFileSync(this.path, 'utf8');
+      const data = await fs.readFile(this.path, 'utf8');
       this.products = JSON.parse(data);
       if (this.products.length > 0) {
         const lastProduct = this.products[this.products.length - 1];
@@ -21,10 +35,10 @@ class ProductManager {
     }
   }
 
-  saveProducts() {
+  async saveProducts() {
     try {
       const data = JSON.stringify(this.products, null, 2);
-      fs.writeFileSync(this.path, data, 'utf8');
+      await fs.writeFile(this.path, data, 'utf8');
       console.log('Productos guardados correctamente.');
     } catch (err) {
       console.log('Error al guardar los productos:', err.message);
@@ -99,46 +113,46 @@ class ProductManager {
   }
 }
 
-const productManager = new ProductManager('products.json');
+async function main() {
+  const productManager = new ProductManager('products.json');
 
-// Agregar un nuevo producto.
-
-productManager.addProduct({
+  // Agregar nuevos productos
+  productManager.addProduct({
     title: 'Producto 1',
     description: 'Descripción del producto 1',
     price: 999.99,
     thumbnail: 'ruta/imagenProducto1.jpg',
     code: 'ABC123',
     stock: 10
-});
+  });
 
-productManager.addProduct({
+  productManager.addProduct({
     title: 'Producto 2',
     description: 'Descripción del producto 2',
     price: 100.50,
     thumbnail: 'ruta/imagenProducto2.jpg',
     code: 'ABC124',
     stock: 7
-});
+  });
 
-productManager.addProduct({
+  productManager.addProduct({
     title: 'Producto 3',
     description: 'Descripción del producto 3',
     price: 199.99,
     thumbnail: 'ruta/imagenProducto3.jpg',
     code: 'ABC125',
     stock: 5
-});
+  });
 
-// Listado de todos los productos
-console.log(productManager.getProducts());
+  // Listado de todos los productos
+  console.log(productManager.getProducts());
 
-// Producto por ID
-console.log(productManager.getProductById(1));
-console.log(productManager.getProductById(7));
+  // Producto por ID
+  console.log(productManager.getProductById(1));
+  console.log(productManager.getProductById(7));
 
-// Actualizar un producto
-productManager.updateProduct(1, { price: 1099.99, stock: 15 });
+  // Actualizar un producto
+  productManager.updateProduct(1, { price: 1999.99, stock: 5 });
+}
 
-// Eliminar un producto
-productManager.deleteProduct(3);
+main();
