3bf1a83a4f571c34c2757f605a06d083eab8b7c2
ismael armando <isarmando82@gmail.com>
2023-09-01 13:44:33 -0300

    detalles2

--- Preentrega2 - Armando/src/public/js/index.js
+++ Preentrega2 - Armando/src/public/js/index.js
@@ -1,15 +1,5 @@
 const url = "http://localhost:8080/products"
 
-function addToCart(comp) {
-    let id = comp.id
-    axios({
-        method: 'post',
-        url: url,
-        data: {
-            id
-        }
-    })
-    .then(data=>console.log(data))
-    .catch(err=>console.log(err))
 
-}
+
+
