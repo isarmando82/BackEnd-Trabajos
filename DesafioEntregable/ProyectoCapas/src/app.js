b19a0739cf008290fbddccf18c7812ef540a7681
ismael armando <isarmando82@gmail.com>
2023-10-04 21:54:34 -0300

    Detalles

--- DesafioEntregable/ProyectoCapas/src/app.js
+++ DesafioEntregable/ProyectoCapas/src/app.js
@@ -108,43 +108,4 @@ socketServer.on('connection', async (socket) => {
 
 
 
-/* //Devuelve todos los productos
-app.get('/productos', async (req, res) => {
-    const productos = await productManager.getProducts();
-    const prodObjeto = JSON.parse(productos); 
-    res.json (prodObjeto);
-}) 
-
-//Devuelve la cantidad de productos segun el query limit
-app.get('/productos/query', async (req, res) => {
-    const {limit} = req.query;
-    if (limit == undefined) {
-        const productos = await productManager.getProducts();
-        const prodObjeto = JSON.parse(productos); 
-        res.json (prodObjeto);
-    }else if(limit > 0){
-        const productos = await productManager.getProducts();
-        const prodObjeto = JSON.parse(productos); 
-        const prodFiltrados = prodObjeto.slice(0, limit);
-        res.json (prodFiltrados);
-    }else {
-        res.json ({error: 'El monto requerido supera la cantidad de productos'});   
-    }
-})
-
-app.get('/productos/:pid', async (req, res) => {
-    const pid = req.params.pid;
-    const id = parseInt(pid);
-    const producto = await productManager.getProductById(id);
-    if (producto) {
-        res.json ({producto});
-    }else {
-        res.json ({error: 'producto no encontrado'});
-    }    
-})
-
-const producto = async () => {
-    await productManager.getProductById(1)
-}
-console.log(producto()); */
 
